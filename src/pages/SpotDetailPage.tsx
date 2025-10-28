fd51f6aa74cdbb8af7839c1b5d4344feeaa9d764
kazukashima <kazutorahattori@gmail.com>
2025-10-28 00:43:38 -0700

    fixed

--- src/pages/SpotDetailPage.tsx
+++ src/pages/SpotDetailPage.tsx
@@ -21,7 +21,7 @@ export default function SpotDetailPage() {
     <div style={{ padding: 20 }}>
       <h2>{spot.name}</h2>
       <p>住所: {spot.address}</p>
-      <p>営業時間: {spot.hours}</p>
+      {/* <p>営業時間: {spot.hours}</p> */}
       {/* 以下の理解 */}
       {spot.photo_url && <img src={spot.photo_url} alt={spot.name} />}
       <a href={`https://maps.google.com/?q=${spot.latitude},${spot.longitude}`}>開く</a>
